773da799ef7713e9f726899483ec2f1988ebe2ed
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-08-24 18:27:26 +0900

    Initial Commit

---
+++ Rock-Paper-Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Lyu Hiroyama on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
